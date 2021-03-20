3e03cb3b0f371de98821ebac2d19625bdbf15971
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-03-20 10:29:41 +0530

    ADD. new new project some changesh

--- src/main/java/bridgelabz/App.java
+++ src/main/java/bridgelabz/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         
-        String message ="Hello World";
+        String message ="Hello World Wold";
         LOG.debug(message + "Will Be Printed On Debug");
         LOG.info(message + "Will Be Printed On Info");
         LOG.warn(message + "Will Be Printed On Warn");
